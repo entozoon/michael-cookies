8687591aed524cb469eec8318a27b037a9a81e8f
Mickey <entozoon@gmail.com>
2023-08-02 16:09:06 +0100

    Dev

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 export const getCookie = (key) => {
   // https://stackoverflow.com/a/21125098/3098773
-  var match = document.cookie.match(new RegExp("(^| )" + key + "=([^;]+)"));
+  let match = document.cookie.match(new RegExp("(^| )" + key + "=([^;]+)"));
   // Return undefined, boolean, or string depending on what comes back
   if (!match || typeof match[2] == "undefined") return undefined;
   const value = match[2];
   return value === "true" ? true : value === "false" ? false : decodeURI(value);
 };
 export const setCookie = ({ key, value, expiryDays }) => {
-  let expiryDate = new Date(Date.now() + expiryDays * 86400000);
-  value =
-    encodeURI(value) +
-    (expiryDays == null ? "" : "; expires=" + expiryDate.toUTCString());
-  document.cookie = key + "=" + value;
+  const expiryDate = new Date(Date.now() + expiryDays * 86400000);
+  let cookieString = `${key}=${encodedValue}`;
+  if (expiryDays) {
+    const expiryString = `;expires=${expiryDate.toUTCString()}`;
+    cookieString += expiryString;
+  }
+  cookieString += ";path=/";
 };
